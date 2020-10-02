bc4cf9bae56293be0497c44c15c0b78d7eafec85
archbloom <bhushanauthankar@gmail.com>
2020-10-02 12:23:54 +0530

    Change share route

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,12 @@ Rails.application.routes.draw do
   root 'home#index'
   get 'home/index'
 
-  resources :notes
+  resources :notes do
+    member do
+      post :share
+    end
+  end
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  post '/share', to: "notes#share"
 end
